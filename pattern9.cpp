fefa865f934c87bf132302ac462863f5adc06b11
Gourav Barnwal <gbgourav2304@gmail.com>
2025-05-27 23:18:55 +0530

    practice

---
+++ pattern9.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i , j ;
+    for(i=1;i<=5;i++){
+        for(j=0;j<+i;j++){
+            cout<<j+10<<" ";
+        }
+        cout<<endl;
+    }
+}
+// output:
+
+//         10 
+//         10 11 
+//         10 11 12 
+//         10 11 12 13 
+//         10 11 12 13 14
