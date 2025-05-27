485577a964e984c032068f47f69b2f6c39fd7121
Gourav Barnwal <gbgourav2304@gmail.com>
2025-05-27 20:30:42 +0530

    more pattern revise

--- boilerplate.cpp
+++ boilerplate.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main(){
-    int i , j ;
-    for(){
-        for(){
+// int main(){
+//     int i , j ;
+//     for(){
+//         for(){
             
-        }
-    }
-}
+//         }
+//     }
+// }
---
+++ pattern4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i , j ;
+    for(i=1 ; i<=5 ; i++){
+        for(j =5; j>=1; j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+// output:
+
+//             5 4 3 2 1 
+//             5 4 3 2 1
+//             5 4 3 2 1
+//             5 4 3 2 1
+//             5 4 3 2 1
