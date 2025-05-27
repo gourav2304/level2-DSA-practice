e2af75b0d9e0a6c0fc03d72400b74ac64b1cfb71
Gourav Barnwal <gbgourav2304@gmail.com>
2025-05-27 23:08:15 +0530

    practice

---
+++ pattern8.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i , j ;
+    int count = 1;
+    for(i=1; i<=5;i++){
+        for(j=1;j<=5;j++){
+            cout<<count<<" ";
+            count = count+1;
+        }
+        cout<<endl;
+    }
+}
+// output:
+
+
+// 1 2 3 4 5 
+// 6 7 8 9 10
+// 11 12 13 14 15
+// 16 17 18 19 20
+// 21 22 23 24 25
