076992601196fc64c2635a14e493facdd464e263
Gourav Barnwal <gbgourav2304@gmail.com>
2025-05-28 18:12:02 +0530

    more dsa question

--- reverseString.js
+++ reverseString.js
@@ -43,26 +43,9 @@ console.log(palindrome("hello"));
 // let result = find(arr,target)
 // console.log("found at ",result);
 
-let arr = [1,2,3,4,5,6,7,8]
-let target = 7 
-function find1(arr, target){
 
-    let start = 1;
-    let end = arr.length-1
 
-    while (start <=end) {
-        let  mid = Math.floor(start+end/2)
 
-        if(arr[mid]==target){
-            return mid
-        }else if(arr[mid]< target){
-             start = mid +1
-        }else{
-            end = mid -1
-        }
-    }
-    return -1
-}
-let result = find1(arr,target)
-console.log("found at ",result);
+
+
 
